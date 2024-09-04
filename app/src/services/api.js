b621879375f1e6ddc3471f678f96311ed71d0db0
ybsetiyawan <ybsetiyawan@gmail.com>
2024-09-04 12:24:42 +0700

    keterangan

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.126.101.217:3000', // Ganti dengan basis URL Anda
+  baseURL: 'http://localhost:3000/', // Ganti dengan basis URL Anda
   // baseURL: 'http://103.217.145.67:4000', // Ganti dengan basis URL Anda
   // opsi lain bisa ditambahkan di sini jika diperlukan
 });
